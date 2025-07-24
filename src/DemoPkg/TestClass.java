2a47ea1a02210d9f6f874ee0c55baa8da042902c
Mohd Daud Alam <mohddaudalam00@gmail.com>
2025-07-24 16:42:59 +0530

    for testing

--- src/DemoPkg/TestClass.java
+++ src/DemoPkg/TestClass.java
@@ -3,5 +3,6 @@ package DemoPkg;
 public class TestClass {
     public static void main(String[] args) {
         System.out.println("ALHUMDULILLAH.");
+        System.out.println("SHUKRANALLAH.");
     }
 }
